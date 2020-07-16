bed4176a163157e30feecaaf4eee7e87dd6787e1
Colin <colinzzz43@gmail.com>
2020-07-16 02:30:24 -0700

    Trying to switch branch

--- src/View/GUI.java
+++ src/View/GUI.java
@@ -1,10 +1,8 @@
 package View;
 
-import java.awt.Dimension;
-import java.awt.GridLayout;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -15,43 +13,76 @@ import javax.swing.Timer;
 
 public class GUI implements ActionListener{
 	
-	private Timer timer;
-	JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
-	JButton stopButton = new JButton("Stop");
-	JButton pauseButton = new JButton("Pause");
-	JButton executeButton = new JButton("Execute");
-	
-	
 	public GUI() {
+		
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JButton stopButton = new JButton("Stop");
+		JButton pauseButton = new JButton("Pause");
+		JButton executeButton = new JButton("Execute");
 
 		
 		// Sensors
+		// Probably make a sensor method to display each data
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		// Text area to show the result
-		JTextArea resultDisplay = new JTextArea(30, 30);
-		resultDisplay.setEditable(false);
-		resultDisplay.setBounds(10, 10, 240, 20);
+		JTextArea displayConsole = new JTextArea(30, 30);
+		displayConsole.setEditable(false);
 		// Scroll panel
-		JScrollPane scrollPane = new JScrollPane(resultDisplay, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		JScrollPane scrollPane = new JScrollPane(displayConsole, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setBounds(10, 20, 960, 500);
+
+		Timer timer = new Timer(500, new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				scrollPane.getVerticalScrollBar().setValue(scrollPane.getVerticalScrollBar().getValue()+10);
+                if (scrollPane.getVerticalScrollBar().getValue()>=scrollPane.getVerticalScrollBar().getMaximum()) {
+                    ((Timer)e.getSource()).stop();
+                }
+			}
+			
+		});
+		
 		
 		// Buttons
-		executeButton.setBounds(260, 10, 100, 20);
-		executeButton.setLocation(100, 50);
+		executeButton.setBounds(370, 700, 100, 20);
 		executeButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// Start printing out the data
 				timer.start();
+				for (int i = 0; i < 5000; i++) {
+					displayConsole.append("Testing\n");
+				}
 			}
 			
 		});
 		
 		
-		stopButton.setBounds(10, 40, 320, 20);
-		stopButton.setLocation(200, 50);
+		pauseButton.setBounds(470, 700, 100, 20);
+		pauseButton.addActionListener(new ActionListener() {
+		
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// Pause the printing service
+			
+			}
+			
+		});
+		
+		
+		stopButton.setBounds(570, 700, 100, 20);
 		stopButton.addActionListener(new ActionListener() {
 		
 			@Override
@@ -62,37 +93,30 @@ public class GUI implements ActionListener{
 			
 		});
 		
-		pauseButton.setBounds(10, 40, 320, 20);
-		pauseButton.setLocation(200, 50);
-		pauseButton.addActionListener(new ActionListener() {
-		
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// Pause the printing service
-				
-			}
-			
-		});
-		
 		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-		panel.setLayout(new GridLayout(2, 1, 3, 3));
+		panel.setBounds(10, 32, 274, 33);
 		
-
-		panel.add(scrollPane);
+//		executeButton.setAlignmentX(Component.LEFT_ALIGNMENT);
+//		executeButton.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+//		pauseButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+//		pauseButton.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+//		stopButton.setAlignmentX(Component.RIGHT_ALIGNMENT);
+//		stopButton.setAlignmentY(Component.RIGHT_ALIGNMENT);
+		
+		
+		panel.setLayout(null);
 		panel.add(executeButton);
 		panel.add(pauseButton);
 		panel.add(stopButton);
-
+		
 		// Basic frame set up
-		frame.getContentPane().add(panel);
-		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Weather sensor");
-		frame.setPreferredSize(new Dimension(500, 500));
-		frame.pack();
+		frame.setSize(1000, 800);
 		frame.setResizable(false);
+		frame.add(scrollPane);
+		frame.add(panel);
 		frame.setVisible(true);
-
 
 	}
 	
@@ -110,3 +134,4 @@ public class GUI implements ActionListener{
 
 
 }
+
