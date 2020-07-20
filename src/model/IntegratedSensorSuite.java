5980fef4a98868289395d68731ca2f1d3776a071
Richard Bankhead <bankhead.richard@gmail.com>
2020-07-20 12:13:31 -0700

    gui overhaul, fully implemented

--- src/model/IntegratedSensorSuite.java
+++ src/model/IntegratedSensorSuite.java
@@ -66,7 +66,7 @@ public class IntegratedSensorSuite implements Serializable{
         initializeSensors();
         initializeThreads();
         startAllThreads();
-        initializeOutputTimer();
+        //initializeOutputTimer();
     }
     
     /**
--- src/view/ConsoleView.java
+++ src/view/ConsoleView.java
@@ -3,177 +3,203 @@ package view;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.AdjustmentEvent;
-import java.awt.event.AdjustmentListener;
 import java.util.Timer;
 import java.util.TimerTask;
-
-import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-
 import application.Main;
 
 public class ConsoleView implements ActionListener{
 	
 	public  Timer timer;
+	JFrame frame;
+	JPanel panel;
+	JPanel universalPanel;
+	JLabel universalLabel;
+	JButton universalButton;
+	JPanel windSpeedPanel;
+	JLabel windSpeedLabel;
+	JLabel windSpeedText;
+	JButton windSpeedButton;
+	JPanel windDirectionPanel;
+	JLabel windDirectionLabel;
+	JLabel windDirectionText;
+	JButton windDirectionButton;
+	JPanel humidityPanel;
+	JLabel humidityLabel;
+	JLabel humidityText;
+	JButton humidityButton;
+	JPanel temperaturePanel;
+	JLabel temperatureLabel;
+	JLabel temperatureText;
+	JButton temperatureButton;
+	JPanel rainPanel;
+	JLabel rainLabel;
+	JLabel rainText;
+	JButton rainButton;
+    public String myCurrentWindDirection;
+    public String myCurrentWindSpeed;
+    public int myCurrentHumidity;
+    public int myCurrentTemperature;
+    public double myCurrentRainAmount;
 	
 	public ConsoleView() {
+
+		//frame setup
+		frame = new JFrame();
+		panel = new JPanel(new GridLayout(2,3));
 		
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel(new GridLayout(2,3));
-		
-		JPanel universalPanel = new JPanel(new GridLayout(2,1));
-		JLabel universalLabel = new JLabel("Start/Stop for ALL sensors:");
-		JButton universalButton = new JButton("Start");
+		universalPanel = new JPanel(new GridLayout(2,1));
+		universalLabel = new JLabel("Start/Stop for ALL sensors:");
+		universalButton = new JButton("Start");
 		universalPanel.add(universalLabel);
 		universalPanel.add(universalButton);
 		
-		JPanel windSpeedPanel = new JPanel(new GridLayout(3,1));
-		JLabel windSpeedLabel = new JLabel("Wind Speed:");
-		JLabel windSpeedText = new JLabel("--");
-		JButton windSpeedButton = new JButton("Start");
+		windSpeedPanel = new JPanel(new GridLayout(3,1));
+		windSpeedLabel = new JLabel("Wind Speed:");
+		windSpeedText = new JLabel("--");
+		windSpeedButton = new JButton("Start");
 		windSpeedPanel.add(windSpeedLabel);
 		windSpeedPanel.add(windSpeedText);
 		windSpeedPanel.add(windSpeedButton);
 		
-		JPanel windDirectionPanel = new JPanel(new GridLayout(3,1));
-		JLabel windDirectionLabel = new JLabel("Wind Direction:");
-		JLabel windDirectionText = new JLabel("--");
-		JButton windDirectionButton = new JButton("Start");
+		windDirectionPanel = new JPanel(new GridLayout(3,1));
+		windDirectionLabel = new JLabel("Wind Direction:");
+		windDirectionText = new JLabel("--");
+		windDirectionButton = new JButton("Start");
 		windDirectionPanel.add(windDirectionLabel);
 		windDirectionPanel.add(windDirectionText);
 		windDirectionPanel.add(windDirectionButton);
 		
-		JPanel humidityPanel = new JPanel(new GridLayout(3,1));
-		JLabel humidityLabel = new JLabel("Humidity:");
-		JLabel humidityText = new JLabel("--");
-		JButton humidityButton = new JButton("Start");
+		humidityPanel = new JPanel(new GridLayout(3,1));
+		humidityLabel = new JLabel("Humidity:");
+		humidityText = new JLabel("--");
+		humidityButton = new JButton("Start");
 		humidityPanel.add(humidityLabel);
 		humidityPanel.add(humidityText);
 		humidityPanel.add(humidityButton);
 		
-		JPanel temperaturePanel = new JPanel(new GridLayout(3,1));
-		JLabel temperatureLabel = new JLabel("Temperature:");
-		JLabel temperatureText = new JLabel("--");
-		JButton temperatureButton = new JButton("Start");
+		temperaturePanel = new JPanel(new GridLayout(3,1));
+		temperatureLabel = new JLabel("Temperature:");
+		temperatureText = new JLabel("--");
+		temperatureButton = new JButton("Start");
 		temperaturePanel.add(temperatureLabel);
 		temperaturePanel.add(temperatureText);
 		temperaturePanel.add(temperatureButton);
 		
-		JPanel rainPanel = new JPanel(new GridLayout(3,1));
-		JLabel rainLabel = new JLabel("Rain Collector:");
-		JLabel rainText = new JLabel("--");
-		JButton rainButton = new JButton("Start");
+		rainPanel = new JPanel(new GridLayout(3,1));
+		rainLabel = new JLabel("Rain Collector:");
+		rainText = new JLabel("--");
+		rainButton = new JButton("Start");
 		rainPanel.add(rainLabel);
 		rainPanel.add(rainText);
 		rainPanel.add(rainButton);
 		
-		
-//		// Text area to show the result
-//		JTextArea displayConsole = new JTextArea(30, 30);
-//		displayConsole.setEditable(false);
-//
-//		// Scroll panel
-//		JScrollPane scrollPane = new JScrollPane(displayConsole, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-//		scrollPane.setBounds(10, 20, 960, 500);
-//
-//		// Auto-scroll for scroll panel 
-//		scrollPane.getVerticalScrollBar().addAdjustmentListener(new AdjustmentListener() {  
-//	        public void adjustmentValueChanged(AdjustmentEvent e) {  
-//	            e.getAdjustable().setValue(e.getAdjustable().getMaximum());  
-//	        }
-//	    });
-		
-		// Buttons
-		//startButton.setBounds(300, 700, 200, 50);
-		//stopButton.setBounds(533, 700, 200, 50);
-		//panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-		//panel.setBounds(10, 32, 274, 33);
-		//panel.setLayout(null);
 		panel.add(windSpeedPanel);
 		panel.add(windDirectionPanel);
 		panel.add(humidityPanel);
 		panel.add(temperaturePanel);
 		panel.add(rainPanel);
 		panel.add(universalPanel);
+		
+		//timer keeping the data refreshed
+		timer = new Timer();
+		timer.schedule(new TimerTask() {
+			@Override
+            public void run() {
+			    myCurrentWindDirection = Main.myIntegratedSensorSuite.myCurrentWindDirection;
+			    myCurrentWindSpeed = Main.myIntegratedSensorSuite.myCurrentWindSpeed;
+			    myCurrentHumidity = Main.myIntegratedSensorSuite.myCurrentHumidity;
+			    myCurrentTemperature = Main.myIntegratedSensorSuite.myCurrentTemperature;
+			    myCurrentRainAmount = Main.myIntegratedSensorSuite.myCurrentRainAmount;
+			    refreshSensorData();
+            }
+		}, 0, 1000); //runs once initially then again every 3 seconds
 
+		
+		//begin ActionListeners for each button
 		universalButton.addActionListener(e -> {
 			if (universalButton.getText().equals("Start")) {
-				//start all sensors
+				windSpeedText.setText(myCurrentWindSpeed);
+				windDirectionText.setText(myCurrentWindDirection);
+				temperatureText.setText(""+myCurrentTemperature);
+				humidityText.setText(""+myCurrentHumidity);
+				rainText.setText(""+myCurrentRainAmount);
 			} else if (universalButton.getText().equals("Stop")) {
-				//stop all sensors
+				windSpeedText.setText("--");
+				windDirectionText.setText("--");
+				temperatureText.setText("--");
+				humidityText.setText("--");
+				rainText.setText("--");
 			}
 			buttonLabelChange(universalButton);
+			buttonLabelChange(windSpeedButton);
+			buttonLabelChange(windDirectionButton);
+			buttonLabelChange(temperatureButton);
+			buttonLabelChange(humidityButton);
+			buttonLabelChange(rainButton);
 		});
 		
 		windSpeedButton.addActionListener(e -> {
 			if (windSpeedButton.getText().equals("Start")) {
-				//start wind speed sensor
+				windSpeedText.setText(myCurrentWindSpeed);
 			} else if (windSpeedButton.getText().equals("Stop")) {
-				//stop wind speed sensor
+				windSpeedText.setText("--");
 			}
 			buttonLabelChange(windSpeedButton);
+			checkUniversalButton();
 		});
 		
 		windDirectionButton.addActionListener(e -> {
 			if (windDirectionButton.getText().equals("Start")) {
-				//start wind direction sensor
+				windDirectionText.setText(myCurrentWindDirection);
 			} else if (windDirectionButton.getText().equals("Stop")) {
-				//stop wind direction sensor
+				windDirectionText.setText("--");
 			}
 			buttonLabelChange(windDirectionButton);
+			checkUniversalButton();
 		});
 		
 		temperatureButton.addActionListener(e -> {
 			if (temperatureButton.getText().equals("Start")) {
-				//start temperature sensor
+				temperatureText.setText(""+myCurrentTemperature);
 			} else if (temperatureButton.getText().equals("Stop")) {
-				//stop temperature sensor
+				temperatureText.setText("--");
 			}
 			buttonLabelChange(temperatureButton);
+			checkUniversalButton();
 		});
 		
 		humidityButton.addActionListener(e -> {
 			if (humidityButton.getText().equals("Start")) {
-				//start humidity sensor
+				humidityText.setText(""+myCurrentHumidity);
 			} else if (humidityButton.getText().equals("Stop")) {
-				//stop humidity sensor
+				humidityText.setText("--");
 			}
 			buttonLabelChange(humidityButton);
+			checkUniversalButton();
 		});
 		
 		rainButton.addActionListener(e -> {
 			if (rainButton.getText().equals("Start")) {
-				//start rain sensor
+				rainText.setText(""+myCurrentRainAmount);
 			} else if (rainButton.getText().equals("Stop")) {
-				//stop rain sensor
+				rainText.setText("--");
 			}
 			buttonLabelChange(rainButton);
+			checkUniversalButton();
 		});
 		
 		// Basic frame set up
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Weather sensor");
-		frame.setSize(800, 600);
+		frame.setSize(600, 400);
 		frame.setResizable(false);
 		frame.add(panel);
 		frame.setVisible(true);
 
-		// Start printing out the data
-//		if (running) return;
-//		timer = new Timer();
-//		timer.schedule(new TimerTask() {
-//			@Override
-//            public void run() {
-//				running = true;
-//				//displayConsole.append(Main.myIntegratedSensorSuite+"\n");
-//            }
-//		}, 1000, 1000); //runs once initially then again every 3 seconds
 	}
 	
 	public void buttonLabelChange(JButton button) {
@@ -181,6 +207,43 @@ public class ConsoleView implements ActionListener{
 			button.setText("Stop");
 		} else if (button.getText().equals("Stop")) {
 			button.setText("Start");
+		}
+	}
+	
+	public void refreshSensorData() {
+		if(!windSpeedText.getText().equals("--")) {
+			windSpeedText.setText(myCurrentWindSpeed);
+		}
+		if(!windDirectionText.getText().equals("--")) {
+			windDirectionText.setText(myCurrentWindDirection);
+		}
+		if(!temperatureText.getText().equals("--")) {
+			temperatureText.setText(""+myCurrentTemperature);
+		}
+		if(!humidityText.getText().equals("--")) {
+			humidityText.setText(""+myCurrentHumidity);
+		}
+		if(!rainText.getText().equals("--")) {
+			rainText.setText(""+myCurrentRainAmount);
+		}
+		
+	}
+	
+	public void checkUniversalButton() {
+		if(windSpeedText.getText().equals("--") 
+				&& windDirectionText.getText().equals("--") 
+				&& temperatureText.getText().equals("--") 
+				&& humidityText.getText().equals("--") 
+				&& rainText.getText().equals("--")) {
+			universalButton.setText("Start");
+		} else {
+			if(!windSpeedText.getText().equals("--") 
+					&& !windDirectionText.getText().equals("--") 
+					&& !temperatureText.getText().equals("--") 
+					&& !humidityText.getText().equals("--") 
+					&& !rainText.getText().equals("--")) {
+				universalButton.setText("Stop");
+			}
 		}
 	}
 
